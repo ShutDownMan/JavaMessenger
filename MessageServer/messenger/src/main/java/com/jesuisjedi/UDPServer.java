ba82ad4ad159784efb99cb7edbff3396600544cf
Gabriel Casseb <ogabrielcasseb@gmail.com>
2022-11-05 17:32:35 -0300

    udp server implemented

--- MessageServer/messenger/src/main/java/com/jesuisjedi/UDPServer.java
+++ MessageServer/messenger/src/main/java/com/jesuisjedi/UDPServer.java
@@ -5,6 +5,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
 
 public class UDPServer {
 
@@ -12,6 +13,8 @@ public class UDPServer {
     private boolean running = false;
     private DatagramSocket socket;
     private byte[] buf = new byte[256];
+
+    private ArrayList<ServerUDPClient> clients = new ArrayList<ServerUDPClient>();
     
     public UDPServer(int port) throws SocketException {
         this.port = port;
@@ -25,12 +28,23 @@ public class UDPServer {
 
         while (running) {
 
-            // Wait for a client connection
+            // Wait for a received packet
             DatagramPacket packet 
             = new DatagramPacket(buf, buf.length);
             this.socket.receive(packet);
 
             System.out.println("Received packet from " + packet.getAddress().getHostAddress() + ":" + packet.getPort());
+            
+            int connectionType = 1;
+            // Check if is connection packet
+            if (connectionType == 2) {
+                ServerUDPClient newClient = new ServerUDPClient(packet.getAddress().getHostAddress(), packet.getPort(), packet.getAddress());
+                clients.add(newClient);
+
+            } else if (connectionType == 3) { // Check if is a disconnection packet
+                ServerUDPClient connectedClient = new ServerUDPClient(packet.getAddress().getHostAddress(), packet.getPort(), packet.getAddress());
+                clients.remove(connectedClient);
+            }
 
             InetAddress address = packet.getAddress();
             int port = packet.getPort();
@@ -38,7 +52,7 @@ public class UDPServer {
             String received 
               = new String(packet.getData(), 0, packet.getLength());
 
-              System.out.println("Received: " + received);
+              System.out.println("Received address:" + packet.getAddress());
             
             if (received.equals("end")) {
                 running = false;
@@ -47,5 +61,22 @@ public class UDPServer {
             this.socket.send(packet);
         }
         this.socket.close();
+    }
+
+    /**
+     * Parse the message received on UDP packet
+     */
+    private void handleMessage() {
+        // TODO
+    }
+
+    // TO DO: Get clients list from UDP packet message on handleMessage
+    public void broadcast() throws IOException {
+        for (ServerUDPClient client : clients) {
+            String message = "Hello from server";
+            
+            DatagramPacket packet = new DatagramPacket(message.getBytes(), message.getBytes().length, client.getIpAddress(), client.getPort());
+            this.socket.send(packet);
+        }
     }
 }
